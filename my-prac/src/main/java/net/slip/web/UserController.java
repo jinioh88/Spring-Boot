5bd417fbf4ae72854c2dd30abe5b5c9427b28f60
OHSEJIN <jinio88@gmail.com>
2018-05-09 01:55:47 +0900

    회원 등록 리스트 / 리다이렉트

--- my-prac/src/main/java/net/slip/web/UserController.java
+++ my-prac/src/main/java/net/slip/web/UserController.java
@@ -1,14 +1,36 @@
 package net.slip.web;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+	
+	List<User> users = new ArrayList<>();
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 
 	@PostMapping("/create")
 	public String create(User user) {
 		System.out.println(user);
-		return "index";  //template
+		if(user!=null) {
+			users.add(user);
+		}
+		return "redirect:list";  //template
+	}
+	
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users",users);
+		return "list";
 	}
 }
