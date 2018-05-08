2fe37bf01e3a9a896503528ceadb1d73cbfd2cd5
OHSEJIN <jinio88@gmail.com>
2018-05-08 22:01:30 +0900

    bootstrap 적용

---
+++ my-prac/src/main/java/net/slip/MyPracApplication.java
@@ -0,0 +1,12 @@
+package net.slip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPracApplication.class, args);
+	}
+}
