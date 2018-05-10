cb449a38d094a5084c160951e66d97792494f872
OHSEJIN <jinio88@gmail.com>
2018-05-10 23:38:30 +0900

    회원가입 / 회원수정 / 목록보기 구현

--- my-prac/src/main/java/net/slip/web/UserController.java
+++ my-prac/src/main/java/net/slip/web/UserController.java
@@ -1,36 +1,61 @@
 package net.slip.web;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slip.domain.User;
+import net.slip.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
-	List<User> users = new ArrayList<>();
+	private List<User> users = new ArrayList<>();
 	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
-
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println(user);
-		if(user!=null) {
-			users.add(user);
-		}
-		return "redirect:list";  //template
+	@Autowired
+	private UserRepository repo;
+	
+	@GetMapping("/form")
+	public String form() {
+		
+		return "/user/form";
 	}
 	
-	@GetMapping("/list")
+	@PostMapping
+	public String add(User user) {
+		users.add(user);
+		repo.save(users);
+		return "redirect:/users";
+	}
+	
+	@GetMapping
 	public String list(Model model) {
-		model.addAttribute("users",users);
-		return "list";
+		model.addAttribute("users",repo.findAll());
+		return "/user/list";
+	}
+	
+	@GetMapping("/{id}")
+	public String update(@PathVariable Long id,Model model) {
+		model.addAttribute("user",repo.findOne(id));
+		return "/user/updateForm";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id,User newUser) {
+		User user = repo.findOne(id);
+		user = newUser;
+		user.setId(id);
+		System.out.println(user);
+		repo.save(user);
+		return "redirect:/users";
 	}
 }
