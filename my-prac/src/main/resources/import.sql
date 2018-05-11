45fa6616497d6ca5cb839c70074014be20afb6a9
OHSEJIN <jinio88@gmail.com>
2018-05-12 03:55:10 +0900

    sql생성/ html 수정

---
+++ my-prac/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into user(user_id, password, name, email) values('oh','1111','세진','oh@gmail.com');
+insert into user(user_id, password, name, email) values('kim','1111','소연','kim@gmail.com');
