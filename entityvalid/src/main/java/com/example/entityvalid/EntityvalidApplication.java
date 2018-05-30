5e0c4fc0cb20821b3e53b73880b9d2d247ea96c5
OHSEJIN <jinio88@gmail.com>
2018-05-31 01:12:14 +0900

    유효성 검증

---
+++ entityvalid/src/main/java/com/example/entityvalid/EntityvalidApplication.java
@@ -0,0 +1,12 @@
+package com.example.entityvalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityvalidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityvalidApplication.class, args);
+    }
+}
