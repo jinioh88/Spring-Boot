8d240ca679b42a3c70a3db44b5afedb96b5a3de6
오세진 <jinioh88@gmail.com>
2018-06-18 13:01:59 +0900

    domain 생

--- part1/boot2/src/main/java/com/example/boot2/domain/Board.java
+++ part1/boot2/src/main/java/com/example/boot2/domain/Board.java
@@ -1,11 +1,32 @@
 package com.example.boot2.domain;
 
+import com.sun.tools.corba.se.idl.constExpr.Times;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.apache.logging.log4j.message.TimestampMessage;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
 
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name="tbl_boards")
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long bno;
+    private String writer;
+    private String title;
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp regdate;
+    @UpdateTimestamp
+    private Timestamp updatedate;
+
 }
