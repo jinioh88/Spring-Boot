6f4e95f2fe15944e6aa9ae8f0d7941d166ae2065
오세진 <jinioh88@gmail.com>
2018-06-19 12:25:35 +0900

    test 코드

--- boot4/src/test/java/com/example/boot4/Boot4ApplicationTests.java
+++ boot4/src/test/java/com/example/boot4/Boot4ApplicationTests.java
@@ -1,16 +1,58 @@
 package com.example.boot4;
 
+import com.example.boot4.domain.Member;
+import com.example.boot4.domain.Profile;
+import com.example.boot4.persistence.MemberRepository;
+import com.example.boot4.persistence.ProfileRepository;
+import lombok.extern.java.Log;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.IntStream;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Log
+@Commit
 public class Boot4ApplicationTests {
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    ProfileRepository profileRepository;
+
     @Test
-    public void contextLoads() {
+    public void testInsert(){
+        IntStream.range(1,101).forEach(i->{
+            Member member = new Member();
+            member.setUid("user"+i);
+            member.setUpw("pw"+i);
+            member.setUname("사용자"+i);
+
+            memberRepository.save(member);
+        });
     }
 
+    @Test
+    public void testInsertProfile(){
+        Member member = new Member();
+        member.setUid("user1");
+
+        for(int i=1;i<5;i++){
+            Profile profile1 = new Profile();
+            profile1.setFname("face"+i+".jpg");
+
+            if(i==1){
+                profile1.setCurrent(true);
+            }
+
+            profile1.setMember(member);
+            profileRepository.save(profile1);
+        }
+    }
 }
