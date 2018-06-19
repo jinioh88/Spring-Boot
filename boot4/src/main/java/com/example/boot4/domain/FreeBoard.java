28c0d69e872569e64cdc0de17f550ecbd3159361
오세진 <jinioh88@gmail.com>
2018-06-19 15:17:49 +0900

    양방향 엔티티 설정

---
+++ boot4/src/main/java/com/example/boot4/domain/FreeBoard.java
@@ -0,0 +1,35 @@
+package com.example.boot4.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="tbl_freeboards")
+@EqualsAndHashCode(of="bno")
+public class FreeBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+    private String title;
+    private String writer;
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp regdate;
+    @UpdateTimestamp
+    private Timestamp updatedate;
+
+    @OneToMany
+    private List<FreeBoardReply> replies;
+}
