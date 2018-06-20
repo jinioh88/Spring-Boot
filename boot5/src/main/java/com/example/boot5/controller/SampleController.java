ef11f53ecab01a1791a3792c86ff006e4f0a11a7
오세진 <jinioh88@gmail.com>
2018-06-20 12:02:03 +0900

     타임리프 : 리스트 출력

--- boot5/src/main/java/com/example/boot5/controller/SampleController.java
+++ boot5/src/main/java/com/example/boot5/controller/SampleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.lang.reflect.Member;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class SampleController {
@@ -19,5 +21,15 @@ public class SampleController {
     public void sample2(Model model){
         MemberVO vo = new MemberVO(123,"u00","p00","호일동",new Timestamp(System.currentTimeMillis()));
         model.addAttribute("vo",vo);
+    }
+
+    @GetMapping("/sample3")
+    public void sample3(Model model){
+        List<MemberVO> list = new ArrayList<>();
+
+        for(int i=0; i<10; i++){
+            list.add(new MemberVO(123,"u0"+i,"p0"+i,"호일동"+i,new Timestamp(System.currentTimeMillis())));
+        }
+        model.addAttribute("list",list);
     }
 }
