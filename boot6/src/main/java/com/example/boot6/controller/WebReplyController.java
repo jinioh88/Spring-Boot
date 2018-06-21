ea96e29f2b0c98556b1809ace5fdc2ced3dbf2f5
오세진 <jinioh88@gmail.com>
2018-06-21 13:52:41 +0900

    댓글 등록

--- boot6/src/main/java/com/example/boot6/controller/WebReplyController.java
+++ boot6/src/main/java/com/example/boot6/controller/WebReplyController.java
@@ -1,5 +1,6 @@
 package com.example.boot6.controller;
 
+import com.example.boot6.domain.WebBoard;
 import com.example.boot6.domain.WebReply;
 import com.example.boot6.persistence.WebReplyRepository;
 import lombok.extern.java.Log;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/replies/*")
@@ -17,11 +20,23 @@ public class WebReplyController {
     private WebReplyRepository replyRepository;
 
     @PostMapping("/{bno}")
-    public ResponseEntity<Void> addReply(@PathVariable("bno")Long bno, @RequestBody WebReply reply){
+    public ResponseEntity<List<WebReply>> addReply(@PathVariable("bno")Long bno, @RequestBody WebReply reply){
         log.info("addReply...");
         log.info("BNO: "+bno);
         log.info("Reply : "+reply);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+
+        reply.setBoard(board);
+        replyRepository.save(reply);
+
+        return new ResponseEntity<>(getListByBoard(board),HttpStatus.CREATED);
     }
+
+    private List<WebReply> getListByBoard(WebBoard board)throws RuntimeException{
+        log.info("getListByBaord..."+board);
+        return replyRepository.getRepliesOfBoard(board);
+    }
+
 }
