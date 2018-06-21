d5e8194bc2bbf8626e748d0aae52e2d658d84bde
오세진 <jinioh88@gmail.com>
2018-06-21 14:31:31 +0900

    수정/리스트호출

--- boot6/src/main/java/com/example/boot6/controller/WebReplyController.java
+++ boot6/src/main/java/com/example/boot6/controller/WebReplyController.java
@@ -7,6 +7,7 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -39,4 +40,28 @@ public class WebReplyController {
         return replyRepository.getRepliesOfBoard(board);
     }
 
+    @Transactional
+    @PutMapping("/{bno}")
+    public ResponseEntity<List<WebReply>> modify(@PathVariable("bno")Long bno, @RequestBody WebReply reply){
+        log.info("modify reply: "+reply);
+
+        replyRepository.findById(reply.getRno()).ifPresent(origin->{
+            origin.setReplyText(reply.getReplyText());
+            replyRepository.save(origin);
+        });
+
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+
+        return new ResponseEntity<>(getListByBoard(board),HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{bno}")
+    public ResponseEntity<List<WebReply>> getReplies(@PathVariable("bno")Long bno){
+        log.info("get All Replies...");
+
+        WebBoard board = new WebBoard();
+        board.setBno(bno);
+        return new ResponseEntity<>(getListByBoard(board),HttpStatus.OK);
+    }
 }
