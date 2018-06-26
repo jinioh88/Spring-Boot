15a75ff84c8a38a1e6eacf8bf72908696dc62dfb
오세진 <jinioh88@gmail.com>
2018-06-26 12:05:06 +0900

    Member, MemberRole

---
+++ src/main/java/com/example/boot08/domain/Member.java
@@ -0,0 +1,31 @@
+package com.example.boot08.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name="tbl_members")
+@EqualsAndHashCode(of="uid")
+@ToString
+public class Member {
+    @Id
+    private String uid;
+    private String upw;
+    private String uname;
+
+    @CreationTimestamp
+    private LocalDateTime regdate;
+    @UpdateTimestamp
+    private LocalDateTime updatedate;
+}
