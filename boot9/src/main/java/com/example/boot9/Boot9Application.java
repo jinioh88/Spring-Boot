c84f90e409f822b907d8eb3bce67c3bd9f6c2995
오세진 <jinioh88@gmail.com>
2018-06-27 12:52:41 +0900

    MVC 시큐리티 통합

---
+++ boot9/src/main/java/com/example/boot9/Boot9Application.java
@@ -0,0 +1,12 @@
+package com.example.boot9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot9Application.class, args);
+    }
+}
---
+++ boot9/src/test/java/com/example/boot9/Boot9ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.boot9;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Boot9ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
