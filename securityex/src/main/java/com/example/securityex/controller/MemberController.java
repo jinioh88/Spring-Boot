3f1c59f1ee5409c540fdfc802cbc3df9b03abeef
오세진 <jinioh88@gmail.com>
2018-08-04 23:04:37 +0900

    암호화

--- securityex/src/main/java/com/example/securityex/controller/MemberController.java
+++ securityex/src/main/java/com/example/securityex/controller/MemberController.java
@@ -6,6 +6,7 @@ import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,8 +27,12 @@ public class MemberController {
 
     }
 
+    @Transactional
     @PostMapping("join")
     public String joinPost(@ModelAttribute("member")Member member) {
+        String encrtpyPw = pwEncoder.encode(member.getUpw());
+        member.setUpw(encrtpyPw);
+        repository.save(member);
         return "/member/joinResult";
     }
 }
